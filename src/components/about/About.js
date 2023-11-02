880dffc9b178e9ee9bcb208c281cd4a5a871b219
Tiago Fernandes <onebirdmen@gmail.com>
2023-11-02 14:49:34 +0000

    uploads

---
+++ src/components/about/About.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Style from './About.module.scss';
+import Terminal from "./Terminal";
+import {Box} from "@mui/material";
+import {info} from "../../info/Info";
+
+
+export default function About() {
+    const firstName = info.firstName.toLowerCase()
+
+    function aboutMeText() {
+        return <>
+            <p><span style={{color: info.baseColor}}>{firstName}{info.lastName.toLowerCase()} $</span> cat
+                about{firstName} </p>
+            <p><span style={{color: info.baseColor}}>about{firstName} <span
+                className={Style.green}>(main)</span> $ </span>
+                {info.bio}
+            </p>
+        </>;
+    }
+
+
+    function miscText() {
+        return <>
+            <p><span style={{color: info.baseColor}}>{firstName}{info.lastName.toLowerCase()} $</span> cd
+                hobbies/interests</p>
+            <p><span style={{color: info.baseColor}}>hobbies/interests <span
+                className={Style.green}>(main)</span> $</span> ls</p>
+            <ul>
+                {info.hobbies.map((hobby, index) => (
+                    <li key={index}><Box component={'span'} mr={'1rem'}>{hobby.emoji}</Box>{hobby.label}</li>
+                ))}
+            </ul>
+        </>;
+    }
+
+    return (
+        <Box display={'flex'} flexDirection={'column'} alignItems={'center'} mt={'3rem'}>
+            <Terminal text={aboutMeText()}/>
+      
+         
+        </Box>
+    )
+}
